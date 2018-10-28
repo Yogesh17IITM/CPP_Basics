479bdd6c4cb798654443dc2dada01c2b991aecff
Yogesh_Windows <yogeshwaranrubin@gmail.com>
2018-10-28 11:17:03 +0530

    Program for greeting msg updated.

--- Prog_3.cpp
+++ Prog_3.cpp
@@ -1,42 +1,71 @@
 /* FUNCTTION EXAMPLES */
 
-/* This is program for displaying greetings 
-
-   welcome() is a function definition
-   with char pointer as an argument.
-
-   Input		: Type your name
-   Expected outout	: Greetings with your name 
-			  in UPPERCASE	
+/* This is a program displaying greeting message both in 
+ * Upper and Lower case.
+ *
+ * Description:
+ *         The function 'is a function definition
+ *         with char pointer as an argument.
+ *
+ * Input		: Enter your name
+ * Expected outout	: Greetings with your name 
+ *                        in UPPERCASE	
 */
-
 #include<iostream>
-
 using namespace std;
-int welcome(char *var);		// Function prototype
 
+/* Function prototypes */
+void GreetMsgUpper(char *ptrUpper);    
+void GreetMsgLower(char *ptrLower);
+
+/* Main Function */
 int main()
 {
-	char name[20];		// Character array
-	char *ptr;		// Character pointer
+	/* Variables declaration */
+	char name[20];		
 
 	cout<<"Enter your name: "<<endl;
 	cin >> name;
-	ptr = name;		// Storing address of name[0] 
 
-	welcome(ptr);		// Function call
-	cout <<name<<endl;	
+	/* Call GreetMsgUpper() to display greet msg
+	 * in Upper case */
+	GreetMsgUpper(name);
+	cout <<"\n Hello, "<<name<<", this is Upper case"<<endl;
+
+	/* Call GreetMsgUpper() to display greet msg
+	 * in Lower case */
+	GreetMsgLower(name);
+	cout <<"\n Hi, "<<name<<", this is Lower case"<<endl;
+
+	return 0;
 }
 
-/* Function for converting lowercase into UPPERCASE */
-int welcome(char *var)
+/* Description:
+ *        Function GreetMsgUpper() displays greet msg in 
+ *        Upper case 
+ */
+void GreetMsgUpper(char *ptrUpper)
 {
-	for(;(*var!='\0');var++)
+	for(;(*ptrUpper!='\0');ptrUpper++)
 	{
-		if( (*var>='a') && (*var<='z') )
+		if( (*ptrUpper>='a') && (*ptrUpper<='z') )
 		{
-			*var = *var-32;
+			*ptrUpper = *ptrUpper-32;
 		}
 	}
-	cout << "welcome, ";	
+}
+
+/* Description:
+ *        Function GreetMsgLower() displays greet msg in 
+ *        Lower case 
+ */
+void GreetMsgLower(char *ptrLower)
+{
+	for(;(*ptrLower!='\0');ptrLower++)
+	{
+		if( (*ptrLower>='A') && (*ptrLower<='Z') )
+		{
+			*ptrLower = *ptrLower+32;
+		}
+	}
 }
