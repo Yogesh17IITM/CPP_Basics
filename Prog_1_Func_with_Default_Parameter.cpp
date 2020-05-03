436dd2631c6232a222a461a702bab7ce4900535f
Yogesh <yogeshwaranrubin@gmail.com>
2020-05-03 11:09:03 +0530

    Updated space intendation

--- Prog_1_Func_with_Default_Parameter.cpp
+++ Prog_1_Func_with_Default_Parameter.cpp
@@ -12,25 +12,25 @@ Key Topics:
 */
 
 // Std Libraries
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 
 // The 'Using' directives
 using namespace std;
 
 // Manifest constants
-#define ENTER(Message) cout<<"[ENTER] "<<Message<<endl
-#define EXIT(Message) cout<<"[EXIT] " <<Message<<endl
+#define ENTER(Message) cout << "[ENTER] " << Message << endl
+#define EXIT(Message) cout << "[EXIT] " << Message << endl
 
-// Function Prototypes 
+// Function Prototypes
 // 1) with default parameters
-int add(int a, 			// Normal parameter
-        int b = 10, 	// Default parameter
-		int c = 20);	// Default parameter
+int add(int a,       // Normal parameter
+        int b = 10,  // Default parameter
+        int c = 20); // Default parameter
 
-// 2) without default parameter 
-int area(int a);						
-int area(int iLength, int iBreadth);	// function overloading (based on #args)
+// 2) without default parameter
+int area(int a);
+int area(int iLength, int iBreadth); // function overloading (based on #args)
 
 /* 
 Note: 
@@ -49,41 +49,44 @@ Note:
 /* Main Program */
 int main()
 {
-	string name;		
-	cout<<"Enter your name: "<<endl;
-	cin >> name;
+    string name;
+    cout << "Enter your name: " << endl;
+    cin >> name;
 
-	// Display Name (in Uppercase)
-	if(0 < name.size())
-	{
-		for (auto &iStr : name)
-			iStr = ::toupper(iStr);
-		cout <<"Hello "<<name<<"\n"<<endl;	
-	}
-	
-	// Demonstrating the usage of Default parameter
-	cout <<"1: Sum is "<<add(100)<<endl;
-	cout <<"2: Sum is "<<add(100, 200)<<endl;
-	cout <<"3: Sum is "<<add(100, 200, 300)<<endl;
-	cout <<"\n4: Area is \n"<<area(5)<<endl;
-	cout <<"\n5: Area is \n"<<area(5, 6)<<endl;
+    // Display Name (in Uppercase)
+    if (0 < name.size())
+    {
+        for (auto &iStr : name)
+            iStr = ::toupper(iStr);
+        cout << "Hello " << name << "\n"
+             << endl;
+    }
 
-	return 0;
+    // Demonstrating the usage of Default parameter
+    cout << "1: Sum is " << add(100) << endl;
+    cout << "2: Sum is " << add(100, 200) << endl;
+    cout << "3: Sum is " << add(100, 200, 300) << endl;
+    cout << "\n4: Area is \n"
+         << area(5) << endl;
+    cout << "\n5: Area is \n"
+         << area(5, 6) << endl;
+
+    return 0;
 }
 
 /* Function definitions */
-int add(int a, int b, int c) { return (a + b + c);}
+int add(int a, int b, int c) { return (a + b + c); }
 
 int area(int a)
 {
-	ENTER("area with one param");	
-	EXIT("area with one param");
-	return (a*a);
+    ENTER("area with one param");
+    EXIT("area with one param");
+    return (a * a);
 }
 
 int area(int iLength, int iBreadth)
 {
-	ENTER("area with two param");	
-	EXIT("area with two param");
-	return iLength * iBreadth;
+    ENTER("area with two param");
+    EXIT("area with two param");
+    return iLength * iBreadth;
 }
