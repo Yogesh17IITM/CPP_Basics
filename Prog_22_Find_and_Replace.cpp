b6c9b0931fe78fbe493484d9115f434c31b9f1a2
Yogesh <yogeshwaranrubin@gmail.com>
2021-04-11 13:26:12 +0530

    Updated Program find and replace

--- Prog_22_Find_and_Replace.cpp
+++ Prog_22_Find_and_Replace.cpp
@@ -6,7 +6,8 @@ int main()
     string input = "abc abc def";
     cout << "Input: " << input << endl;
 
-    // find bc and replace with ???
+    // Topic: Find and replace
+    // Problem: find 'bc' and replace with ???
     string strToReplace = "bc";
     string strReplaceBy = "???";
 
@@ -17,10 +18,18 @@ int main()
         input.replace(pos, strToReplace.length(), strReplaceBy);
         pos += strReplaceBy.length() + 1;
     }
+    cout << "Output 1: " << input << endl;
 
-    /*pos = input.find(strToReplace, pos);
-    input.replace(pos, strToReplace.length(), strReplaceBy);*/
+    // Topic: Find and erase
+    // Problem: find '???' and erase it
+    string strToErase = "???";
+    auto posToErase = 0;
+    while ((posToErase = input.find(strToErase, posToErase)) &&
+        posToErase != string::npos)
+    {
+        input.erase(posToErase, strToErase.length());
+    }
+    cout << "Output 2: " << input << endl;
 
-    cout << "Output: " << input << endl;
     return 0;
 }
