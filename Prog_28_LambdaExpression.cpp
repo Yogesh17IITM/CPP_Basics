496e23df9c8778fe9c6caadb7df358407ec51cb0
Yogesh <yogeshwaranrubin@gmail.com>
2021-04-29 20:38:28 +0530

    Lambda Expression

---
+++ Prog_28_LambdaExpression.cpp
@@ -0,0 +1,43 @@
+/*
+	Extract characters from given strings and 
+	store it into a container
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+	string str1 = "Hai";
+	string str2 = " Hello";
+
+	// Traditional way
+	vector<char> chArr;
+	for (int idx = 0; idx < str1.length(); idx++)
+		chArr.push_back(str1[idx]);
+
+	for (int idx = 0; idx < str2.length(); idx++)
+		chArr.push_back(str2[idx]);
+
+	for (auto& iCh : chArr)
+		cout << iCh;
+	cout << endl;
+
+	cout << "\nUsing Lamda:" << endl;
+
+	// Using Lambda Expression	
+	chArr.clear();
+	auto PushBack = [&](string iStr){		
+		for (int idx = 0; idx < iStr.length(); idx++)
+			chArr.push_back(iStr[idx]);		
+	};
+
+	PushBack(str1);
+	PushBack(str2);
+	for (auto& iCh : chArr)
+		cout << iCh;
+	cout << endl;
+
+	return 0;
+}
