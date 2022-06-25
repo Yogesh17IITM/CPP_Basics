336cb1b9b7e6f9f61ff9a80c5a27d40f4f369259
Yogesh17IITM <yogeshwaranrubin@gmail.com>
2022-06-25 12:45:56 +0530

    Update Prog_1_Simple_Cpp_Program.cpp
    Update code for readability. Added validations.

--- Prog_1_Simple_Cpp_Program.cpp
+++ Prog_1_Simple_Cpp_Program.cpp
@@ -1,38 +1,59 @@
 /* Simple C++ Program */
 
-/* Program for converting 12 hrs format into 24 hrs format 
+/* Program for converting 12 hrs format into 24 hrs format
    Input	: Time in HH:MM AM/PM
-   Output	: Equivalent time in 24 hrs format   
+   Output	: Equivalent time in 24 hrs format
 */
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-int main()
+
+string ConvertTime_12To24Hrs(const string &iStrTimeNow)
 {
-	string strTime = "12:20AM";	
-	
-	string strOut;
-    string strHour = strTime.substr(0,2);    
-	
-	if(0 == strTime.compare(5, 2, "PM"))
-	{		
-        // Handle for 12:MMPM	
-        if(0 != strTime.compare(0, 2,"12"))
+    string strHour_12 = iStrTimeNow.substr(0, 2);
+    string strMin = iStrTimeNow.substr(3, 2);
+
+    // Check is AM?
+    string strHour_24 = "";
+    if ("AM" == iStrTimeNow.substr(6, 2))
+    {
+        strHour_24 = ("12" == strHour_12) ? "00" : strHour_12;
+    }
+    else // it is PM
+    {
+        if ("12" == strHour_12)
+            strHour_24 = strHour_12;
+        else
         {
-		    int Hour = ::stoi(strHour) + 12;
-		    strHour = ::to_string(Hour);		
+            int Hour_24 = ::stoi(strHour_12) + 12;
+            strHour_24 = ::to_string(Hour_24);
         }
-	}
-	else
-	{
-        // Handle for 12:MMAM
-		if(0 == strTime.compare(0,2,"12"))
-            strHour = "00";
-	}
-    strOut = strHour+strTime.substr(2,3);		        
-    
-	cout<<strOut<<endl;	
-	return 0;
+    }
+
+    return (strHour_24 + iStrTimeNow.substr(2, 3));
 }
 
+int main()
+{
+    string strTimeNow1 = "12:00 AM";
+    string strTimeNow2 = "12:00 PM";
+    string strTimeNow3 = "01:00 AM";
+    string strTimeNow4 = "01:00 PM";
+
+    cout << "Time (in 24 hrs) is:    " << ConvertTime_12To24Hrs(strTimeNow1) << endl;
+    cout << "Time (in 24 hrs) is:    " << ConvertTime_12To24Hrs(strTimeNow2) << endl;
+    cout << "Time (in 24 hrs) is:    " << ConvertTime_12To24Hrs(strTimeNow3) << endl;
+    cout << "Time (in 24 hrs) is:    " << ConvertTime_12To24Hrs(strTimeNow4) << endl;
+
+    // Validate results
+    if (("00:00" == ConvertTime_12To24Hrs(strTimeNow1)) &&
+        ("12:00" == ConvertTime_12To24Hrs(strTimeNow2)) &&
+        ("01:00" == ConvertTime_12To24Hrs(strTimeNow3)) &&
+        ("13:00" == ConvertTime_12To24Hrs(strTimeNow4)))
+        cout << "Test results PASSED";
+    else
+        cout << "Test results FAILED";
+
+    return 0;
+}
