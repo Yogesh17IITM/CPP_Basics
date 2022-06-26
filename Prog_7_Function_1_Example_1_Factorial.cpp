7941c9f030e9cde50983683754f54f907fd3a730
Yogesh17IITM <yogeshwaranrubin@gmail.com>
2022-06-26 18:14:24 +0530

    Update Prog_7_Function_1_Example_1_Factorial.cpp
    Added recursive method to calculate result

--- Prog_7_Function_1_Example_1_Factorial.cpp
+++ Prog_7_Function_1_Example_1_Factorial.cpp
@@ -1,35 +1,45 @@
 /* Function Example */
 
-/* Program for finding factorial 
+/* Program for finding factorial
 
    Input	    : Type a number for finding Factorial
    Exp. output	: Factorial of that given no.
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 // Function Prototype
 long unsigned int fact(int iNum);
+long unsigned int recursiveFact(int iNum); // Calc. using recurive mechanism
 
 int main()
 {
-	int num;
-	cout<<"Enter a number: "<<endl;
-	cin>>num;
-	
-	cout<<"The Factorial of "<<num<<" is : "<<fact(num)<<endl;
-	return 0;
+    int num;
+    cout << "Enter a number: " << endl;
+    cin >> num;
+
+    cout << "The Factorial of " << num << " is : " << fact(num) << endl;
+    cout << "The Factorial of " << num << " is : " << recursiveFact(num) << endl;
+    return 0;
 }
 
 // Function Definition
 long unsigned int fact(int iNum)
 {
-	long unsigned int res = 1;
-    while(iNum > 0)
+    long unsigned int res = 1;
+    while (iNum > 0) // O(N)
     {
         res = res * iNum;
         iNum--;
     }
-	return res;
+    return res;
+}
+
+long unsigned int recursiveFact(int iNum) // O(N)
+{
+    if (0 == iNum)
+        return 1;
+
+    return (iNum * recursiveFact(iNum - 1));
 }
