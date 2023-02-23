ee80344e3314b6c39e31286ff56641aecdaa34a0
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2023-02-23 13:05:44 +0530

    Updated logic

--- Prog_4_BitwiseOperators_1_Intro.cpp
+++ Prog_4_BitwiseOperators_1_Intro.cpp
@@ -1,74 +1,73 @@
-/* 
-Objective: 
-	To demonstrate bitwise operations
+/*
+Objective:
+    To demonstrate bitwise operations
 
 Example:
-	String comparision using bitwise opeation (XOR)
+    String comparision using bitwise opeation (XOR)
 
 Key Topics:
-	1) Char array, Pointers
+    1) Char array, Pointers
     2) String comparision
     3) bitwise operations : AND, OR, XOR, Left and Right Shift
 */
 
 // Std Libraries
-#include<iostream>
+#include <iostream>
 
 // The 'Using' directive
 using namespace std;
 
 // Function declaration
-void CompareChar(char * iChA, char * iChB);
+void CompareChar(char *iChA, char *iChB);
 
 /* Main program */
 int main()
 {
-    unsigned int a=5, b=6; // (5 = 0101, 6 = (0110)
+    unsigned int a = 5, b = 6; // (5 = 0101, 6 = (0110)
     int c = 0;
-    cout <<a<<" "<<b<<endl;
+    cout << a << " " << b << endl;
 
     // Bitwise operations
     cout << "1) AND: " << (a & b) << endl;          // returns 1 only if both bits are 1
     cout << "2) OR: " << (a | b) << endl;           // returns 1 if any of the two bits is 1.
-    cout << "3) XOR: " << (a^b) << endl;            // returns 1 if the two bits are different.
-    cout << "4) Left shift: "<< (a << 2) <<endl;    /* left shifts the bits of the first operand, 
-                                                       the second operand decides the number of places to shift. 
+    cout << "3) XOR: " << (a ^ b) << endl;          // returns 1 if the two bits are different.
+    cout << "4) Left shift: " << (a << 2) << endl;  /* left shifts the bits of the first operand,
+                                                       the second operand decides the number of places to shift.
                                                        010100 = 20 */
-    cout << "5) Right shift: "<< (a >> 2) << endl;  /* right shifts the bits of the first operand, 
-                                                       the second operand decides the number of places to shift. 
+    cout << "5) Right shift: " << (a >> 2) << endl; /* right shifts the bits of the first operand,
+                                                       the second operand decides the number of places to shift.
                                                        0001 = 1 */
     char chA[] = "Hello";
     char chB[] = "Hello";
     cout << "Comparing strings 1: ";
     CompareChar(chA, chB);
 
-    char chC[] = "Helo";
+    char chC[] = "Hell";
     cout << "Comparing strings 2: ";
     CompareChar(chA, chC);
     return 0;
 }
 
 // Function definition
-void CompareChar(char * iChA, char * iChB)
+void CompareChar(char *iChA, char *iChB)
 {
-    bool bRc = false;
-    while (*iChA != '\0' || *iChB != '\0')
+    bool bIsSame = true;
+    while ((*iChA != '\0') && (*iChB != '\0') && bIsSame == true)
     {
-        if (*iChA == '\0' || *iChB == '\0')        
-            break;        
+        if (0 != (*iChA ^ *iChB))
+            bIsSame = false;
 
-        if(0 == (*iChA ^ *iChB)) {  bRc = true; }
-        else  
-        {     
-            bRc = false;
-            break;
-        }
-                                
         iChA++;
         iChB++;
     }
-    
+
+    // check if next char still exists in either of char array
+    if (*iChA != '\0' || *iChB != '\0')
+        bIsSame = false;
+
     // Display results
-    if(bRc) cout <<"Strings are equal"<<endl;
-    else cout <<"Strings are NOT equal"<<endl;
+    if (bIsSame)
+        cout << "Strings are equal" << endl;
+    else
+        cout << "Strings are NOT equal" << endl;
 }
