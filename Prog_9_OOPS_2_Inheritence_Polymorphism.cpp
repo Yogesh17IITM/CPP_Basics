aaaf4f2b3cb2247f61ba32a9fc4757f9edb401f3
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2023-03-10 16:46:34 +0530

    Updated function access for multiple-multilevel obj

--- Prog_9_OOPS_2_Inheritence_Polymorphism.cpp
+++ Prog_9_OOPS_2_Inheritence_Polymorphism.cpp
@@ -1,71 +1,74 @@
 /* C++ Inheritence */
-#include<iostream>
+#include <iostream>
 using namespace std;
-static int stCount=0;    // Notice static cannot be used inside class
+static int stCount = 0; // Notice static cannot be used inside class
 
-class parent 
+class parent
 {
-	int count=0;
-	public:
-		parent()    // constructor
-		{
-			++count;
-			++stCount;    // Retains value even it goes out of the scope
-			cout<<count<<" Parent constructor "<<stCount<<endl;
-		}
+    int count = 0;
 
-		virtual void Greet()
-		{
-			cout << "Parent" <<endl;
-		}
-};		
+public:
+    parent() // constructor
+    {
+        ++count;
+        ++stCount; // Retains value even it goes out of the scope
+        cout << count << " Parent constructor " << stCount << endl;
+    }
+
+    virtual void Greet()
+    {
+        cout << "Parent" << endl;
+    }
+};
 
 class first : public parent
 {
-	public:
-		void Greet() override
-		{
-			cout <<"first "<<endl;
-		}
+public:
+    void Greet() override
+    {
+        cout << "first " << endl;
+    }
 };
 
 class second : private parent
 {
-	public:
-		void Greet() override
-		{
-			cout <<"second "<<endl;
-		}
+public:
+    void Greet() override
+    {
+        cout << "second " << endl;
+    }
 };
 
 class third : public parent
 {
-	// Notice how function call happens
+    // Notice how function call happens
 };
 
-class multiple : public first, public second  // Multiple Inheritence
+class multiple : public first, public second // Multiple Inheritence
 {
-	public:
-		multiple()
-		{  cout<<"Multiple Constructor "<<endl;} 
+public:
+    multiple()
+    {
+        cout << "Multiple Constructor " << endl;
+    }
 };
-
 
 int main()
 {
-	// Creating Objects for each class
-	parent oParent;
-	first oFirst;
-	second oSecond;
-	third oThird;
-	cout<<"\n Multiple inheritence"<<endl;
-	multiple oMultiple;
+    // Creating Objects for each class
+    parent oParent;
+    first oFirst;
+    second oSecond;
+    third oThird;
+    cout << "\n Multiple inheritence" << endl;
+    multiple oMultiple;
 
-	// Func. call
-	oParent.Greet();
-	oFirst.Greet();
-	oSecond.Greet();
-	oThird.Greet();
+    // Func. call
+    oParent.Greet();
+    oFirst.Greet();
+    oSecond.Greet();
+    oThird.Greet();
+    // oMultiple.Greet(); compilation error due to ambiguity
 
-	return 0;
+    return 0;
 }
