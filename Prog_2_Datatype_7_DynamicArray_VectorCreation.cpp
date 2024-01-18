0fcf7562a1442ebd990fc0c2b53e4edd44264ed9
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2024-01-19 01:42:29 +0530

    Updated code

--- Prog_2_Datatype_7_DynamicArray_VectorCreation.cpp
+++ Prog_2_Datatype_7_DynamicArray_VectorCreation.cpp
@@ -1,69 +1,59 @@
 // Dynamic array Creation - VECTOR creation
 
 #include <iostream>
+#include <cstring>
 using namespace std;
-
-#define DEFAULT_SIZE 1
 
 // Creating customized VECTOR class using template (i.e., class template)
 template <typename T>
 class MyVector
 {
-    T *_Ptr = nullptr;
-    int _Size;
+    T *_pData = nullptr;
+    int _size = 1;
 
 public:
-    MyVector();
-    void push_back(T iVal);
+    void push_back(const T &iVal)
+    {
+        if (!_pData)
+        {
+            _pData = new T[_size]{iVal}; // use { } to initialize array list
+        }
+        else
+        {
+            T *pNewData = new T[_size + 1];
+
+            // use memcpy to copy one array data into another of definite size.
+            memcpy(pNewData, _pData, _size * sizeof(int)); // notice: _size * sizeof(int)
+
+            delete[] _pData;
+            _pData = pNewData; // updated pointer of new size => size+1 with old data.
+
+            // update the new incoming data
+            _pData[_size++] = iVal; // assign the input value at size and then increment the size to +1.
+        }
+    }
 
     /* Similarly you can implement pop_back, size, etc., */
 
-    void PrintValues();
+    void PrintValues()
+    {
+        try
+        {
+            if (!_pData)
+                throw -999;
+
+            for (int idx = 0; idx < _size; idx++)
+            {
+                cout << _pData[idx] << " ";
+            }
+            cout << endl;
+        }
+        catch (int &e)
+        {
+            cout << "Nothing to print. No data available." << endl;
+        }
+    }
 };
-
-template <typename T>
-MyVector<T>::MyVector()
-{
-    _Size = DEFAULT_SIZE;
-}
-
-template <typename T>
-void MyVector<T>::push_back(T iVal)
-{
-    if (nullptr == _Ptr)
-        _Ptr = new T[_Size]{iVal};
-    else
-    {
-        // 1) Create New Arr and update values
-        T *NewPtr = new T[_Size + 1]; // increment size by 1
-        for (int idx = 0; idx < _Size; idx++)
-            NewPtr[idx] = _Ptr[idx];
-        NewPtr[_Size] = iVal;
-
-        // 2) Update new size
-        _Size++;
-
-        // 3) Delete existing arr and update to new array
-        if (_Ptr)
-        {
-            delete[] _Ptr;
-            _Ptr = NewPtr;
-        }
-    }
-}
-
-template <typename T>
-void MyVector<T>::PrintValues()
-{
-    if (_Ptr)
-    {
-        for (int idx = 0; idx < _Size; idx++)
-        {
-            cout << _Ptr[idx] << " ";
-        }
-        cout << endl;
-    }
-}
 
 int main()
 {
@@ -74,4 +64,7 @@ int main()
     vec.push_back(45);
     vec.push_back(56);
     vec.PrintValues();
+
+    MyVector<int> emptyVec;
+    emptyVec.PrintValues();
 }
