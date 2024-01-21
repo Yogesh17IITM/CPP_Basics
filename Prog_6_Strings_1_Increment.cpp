f789d5f4cbd1ab455288402680b4f7ddc5a2412a
YOGESH17IITM <yogeshwaranrubin@gmail.com>
2024-01-21 15:55:41 +0530

    updated cipher code

--- Prog_6_Strings_1_Increment.cpp
+++ Prog_6_Strings_1_Increment.cpp
@@ -8,8 +8,12 @@ Description:
 For example,
     With a shift of 1, A would be replaced by B, B would become C, and so on.
 
-Input  : ABC DEF1
-Output : XYZ ABC1
+Input:
+    string:     ZAB DezF1
+    shiftIdx:   1
+
+Output:
+    ABC EfaG1
 */
 
 #include <iostream>
@@ -18,38 +22,62 @@ using namespace std;
 // Spcify the Shift Index (i.e., No. of positions to be shifted)
 #define SHIFT_IDX 1
 
-string EncryptString(const string &iStr, int iSftIdx)
+string EncryptedString(const string &str, int idx)
 {
-    string oEncryptStr = "";
-    for (auto &iCh : iStr)
+    string ostr = "";
+    try
     {
-        // Check if it is of non-char type. If so, append as it is.
-        if (!(::isalpha(iCh)))
+        if (str.empty())
+            throw -1;
+
+        ostr = str;
+        if (idx == 0)
         {
-            oEncryptStr += iCh;
-            continue; // skip the below steps and continue to next iteration
+            throw -2;
         }
 
-        // Determine the start index based on the case (upper/lower)
-        int Start_Idx = (::isupper(iCh)) ? (int)'A' : (int)'a'; // For upper 'A' -> 65 and for lower 'a' -> 97
-
-        // Calculate the new idx (New_Idx-Start_idx -> should be cyclic between 0 to 26)
-        int New_Idx = ((int)iCh + iSftIdx - Start_Idx) % 26 + Start_Idx;
-
-        // Cast the new index interger value into char type
-        oEncryptStr += (char)New_Idx;
+        for (auto &iCh : ostr)
+        {
+            // check if it is letter
+            if (::isalpha(iCh))
+            {
+                int chVal = 0;
+                char startLetter = ::isupper(iCh) ? 'A' : 'a';
+                chVal = ((int)iCh + idx - (int)startLetter) % 26 + (int)startLetter;
+                iCh = (char)chVal;
+            }
+            else
+            {
+                // it can be digits/special characters/space
+                continue;
+            }
+        }
+    }
+    catch (const int &eRet)
+    {
+        if (eRet == -1)
+            cout << " \n[ERROR] Empty string provided" << endl;
+        else if (eRet == -2)
+            cout << " \n[WARNING] Shift index is zero" << endl;
+        else
+        {
+        }
     }
 
-    return oEncryptStr;
+    return ostr;
 }
 
 int main()
 {
     string str = "ZAB DezF1";
-    string strEncrypted = EncryptString(str, SHIFT_IDX);
+    int shiftIdx = 1;
+    cout << EncryptedString(str, shiftIdx) << endl; // Notice: 'Z' and 'z' shifting to valid char instead of some undefined symbols
 
-    // Display Encrpted Message
-    cout << strEncrypted << endl; // Notice: 'Z' and 'z' shifting to valid char instead of some undefined symbols
+    shiftIdx = 0;
+    cout << EncryptedString(str, shiftIdx) << endl;
+
+    str = "";
+    cout << EncryptedString(str, shiftIdx) << endl;
 
     return 0;
 }
